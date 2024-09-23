2a3c1d955e08c1ac11178d3ab2200a9dc6a627e9
2202210 <2202210@sit.singaporetech.edu.sg>
2024-09-23 21:54:33 +0800

    add files for first exercise

--- blink.c
+++ blink.c
@@ -32,8 +32,8 @@ int pico_led_init(void)
 #define UART_ID uart1
 #define BAUD_RATE 115200
 
-#define UART_TX_PIN 8  // GP8 (TX)
-#define UART_RX_PIN 9  // GP9 (RX)
+#define UART_TX_PIN_8 8  // GP8 (TX)
+#define UART_RX_PIN_9 9  // GP9 (RX)
 #define BTN_PIN 22     // GP22 (Button)
 
 void setup_uart() 
@@ -42,8 +42,8 @@ void setup_uart()
     uart_init(UART_ID, BAUD_RATE);
     
     // Set GPIO pins for UART1 TX (GP8) and RX (GP9)
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_TX_PIN_8, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN_9, GPIO_FUNC_UART);
 }
 
 void setup_button() 
@@ -122,37 +122,97 @@ void loop() {
     }
 }
 
-// Send a character 'A' via UART every 2 sec
-void loopback_test()
-{
-    while (true)
-	{
-        uart_putc(UART_ID, 'A');
-        sleep_ms(100);
-        if (uart_is_readable(UART_ID)) 
-		{
-            char received = uart_getc(UART_ID);
-            printf("Received: %c\n", received);
-        } 
-		else 
-            printf("No data received!\n");
-        sleep_ms(1000);
+// // Send a character 'A' via UART every 2 sec
+// void loopback_test()
+// {
+//     while (true)
+// 	{
+//         uart_putc(UART_ID, 'A');
+//         sleep_ms(500);
+//         if (uart_is_readable(UART_ID)) 
+// 		{
+//             char received = uart_getc(UART_ID);
+//             printf("Received: %c\n", received);
+//         } 
+// 		else 
+//             printf("No data received!\n");
+//         sleep_ms(1000);
+//     }
+// }
+
+// lab3
+static char event_str[128];
+
+void gpio_event_string(char *buf, uint32_t events);
+
+void gpio_callback(uint gpio, uint32_t events) {
+    // Put the GPIO event(s) that just happened into event_str
+    // so we can print it
+    gpio_event_string(event_str, events);
+    printf("GPIO %d %s\n", gpio, event_str);
+}
+
+static const char *gpio_irq_str[] = {
+        "LEVEL_LOW",  // 0x1
+        "LEVEL_HIGH", // 0x2
+        "EDGE_FALL",  // 0x4
+        "EDGE_RISE"   // 0x8
+};
+
+void gpio_event_string(char *buf, uint32_t events) {
+    for (uint i = 0; i < 4; i++) {
+        uint mask = (1 << i);
+        if (events & mask) {
+            // Copy this event string into the user string
+            const char *event_str = gpio_irq_str[i];
+            while (*event_str != '\0') {
+                *buf++ = *event_str++;
+            }
+            events &= ~mask;
+
+            // If more events add ", "
+            if (events) {
+                *buf++ = ',';
+                *buf++ = ' ';
+            }
+        }
     }
+    *buf++ = '\0';
 }
 
 int main() {
 	stdio_init_all();  // Initialize USB serial
     int rc = pico_led_init();
     hard_assert(rc == PICO_OK);
-
+	
+	//set 1
 	//testing loopback
 	// This sends and check if a is received.
-    // setup_uart();       // Set up UART1 for communication
-    // loopback_test();    // Enter the loopback test function
+		// setup_uart();       // Set up UART1 for communication
+		// loopback_test();    // Enter the loopback test function
 	// end of testing
 
- 	setup_uart();       // Set up UART1 for communication
-    setup_button();     // Set up the button (GP22)
-    loop();  // Enter the main loop
+	//set2
+ 	// setup_uart();       // Set up UART1 for communication
+    // setup_button();     // Set up the button (GP22)
+    // loop();  // Enter the main loop
+
+	printf("Hello GPIO IRQ\n");
+	//  This will set up the interrupt to detect button presses on GPIO 20.
+	// the onboard button on the Maker Pi Pico is physically connected to GP20,
+	//  you do not need to connect any external wires for it to work. The button will trigger interrupts directly on GPIO 20 (GP20).
+	// only for GP20
+    gpio_set_irq_enabled_with_callback(20, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+
+
+	// For level low (trigger when the pin is held low):
+	// gpio_set_irq_enabled_with_callback(20, GPIO_IRQ_LEVEL_LOW, true, &gpio_callback);
+    
+	// For level high (trigger when the pin is held high):
+	// gpio_set_irq_enabled_with_callback(20, GPIO_IRQ_LEVEL_HIGH, true, &gpio_callback);
+	
+	// Wait forever
+    while (1);
+
     return 0;
 }
